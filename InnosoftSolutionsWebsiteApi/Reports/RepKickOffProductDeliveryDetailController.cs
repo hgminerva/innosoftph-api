121a7c18f1aba8a16772cb3e16bd7ea33c3741a3
NoahOliverRigonan <oliverrigonan@gmail.com>
2017-03-27 15:29:54 +0800

    Delivery PDF Changes

--- InnosoftSolutionsWebsiteApi/Reports/RepKickOffProductDeliveryDetailController.cs
+++ InnosoftSolutionsWebsiteApi/Reports/RepKickOffProductDeliveryDetailController.cs
@@ -1,7 +1,9 @@
 ﻿using iTextSharp.text;
+using iTextSharp.text.html;
 using iTextSharp.text.pdf;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -33,7 +35,6 @@ namespace InnosoftSolutionsWebsiteApi.Reports
         public String DocumentNumber { get; set; }
         public String ContactPerson { get; set; }
         public String ContactPersonPhoneNumber { get; set; }
-        public String ContactPersonAddress { get; set; }
         public List<PrintDeliveryProductList> ProductLists { get; set; }
         public String Particulars { get; set; }
         public List<PrintDeliveryCheckList> CheckLists { get; set; }
@@ -42,6 +43,7 @@ namespace InnosoftSolutionsWebsiteApi.Reports
         public String TechnicalUser { get; set; }
         public String FunctionalUser { get; set; }
         public String CustomerUser { get; set; }
+        public String KickOffDeliveryDate { get; set; }
     }
 
     // PDF
@@ -60,7 +62,8 @@ namespace InnosoftSolutionsWebsiteApi.Reports
                 Rectangle rectangle = new Rectangle(PageSize.A3);
                 Document document = new Document(rectangle, 72, 72, 72, 72);
                 document.SetMargins(50f, 50f, 50f, 50f);
-                PdfWriter.GetInstance(document, workStream).CloseStream = false;
+                PdfWriter writer = PdfWriter.GetInstance(document, workStream);
+                writer.CloseStream = false;
 
                 // Document open
                 document.Open();
@@ -73,6 +76,7 @@ namespace InnosoftSolutionsWebsiteApi.Reports
                 Font fontArial12 = FontFactory.GetFont("Arial", 12);
                 Font fontArial11Bold = FontFactory.GetFont("Arial", 11, Font.BOLD);
                 Font fontArial11 = FontFactory.GetFont("Arial", 11);
+                Font fontArial11White = FontFactory.GetFont("Arial", 11, BaseColor.WHITE);
                 Font fontArial11ITALIC = FontFactory.GetFont("Arial", 12, Font.ITALIC);
                 Font fontArial10Bold = FontFactory.GetFont("Arial", 10, Font.BOLD);
                 Font fontArial10 = FontFactory.GetFont("Arial", 10);
@@ -110,6 +114,117 @@ namespace InnosoftSolutionsWebsiteApi.Reports
                 quotationDetailLineHeader.AddCell(new PdfPCell(new Phrase("", fontArial17Bold)) { Border = 1 });
                 document.Add(quotationDetailLineHeader);
 
+                // line
+                document.Add(line);
+
+                // hex header color
+                BaseColor headerTableColorHeader = WebColors.GetRGBColor("#112540");
+
+                // table data
+                PdfPTable kickOffDeliveryDataTable = new PdfPTable(4);
+                float[] kickOffDeliveryDataTableWithCells = new float[] { 25f, 25f, 25f, 25f };
+                kickOffDeliveryDataTable.SetWidths(kickOffDeliveryDataTableWithCells);
+                kickOffDeliveryDataTable.WidthPercentage = 100;
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(new Phrase("Innosoft Form No.: " + deliveryObjectList.FirstOrDefault().ISFormNumber + " (Kick-off Meeting Document)", fontArial11White)) { Colspan = 4, PaddingTop = 10f, PaddingBottom = 12f, PaddingLeft = 5f, PaddingRight = 5f, HorizontalAlignment = 1, BackgroundColor = headerTableColorHeader });
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(new Phrase("Document No.: " + deliveryObjectList.FirstOrDefault().DocumentNumber, fontArial11)) { PaddingTop = 7f, PaddingBottom = 9f, PaddingLeft = 5f, PaddingRight = 5f });
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(new Phrase("Date: " + deliveryObjectList.FirstOrDefault().KickOffDeliveryDate, fontArial11)) { PaddingTop = 7f, PaddingBottom = 9f, PaddingLeft = 5f, PaddingRight = 5f });
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(new Phrase("Prepared By: " + deliveryObjectList.FirstOrDefault().PreparedByUser, fontArial11)) { Colspan = 2, PaddingTop = 7f, PaddingBottom = 9f, PaddingLeft = 5f, PaddingRight = 5f });
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(new Phrase("Customer: " + deliveryObjectList.FirstOrDefault().Customer, fontArial11)) { Colspan = 2, PaddingTop = 7f, PaddingBottom = 9f, PaddingLeft = 5f, PaddingRight = 5f });
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(new Phrase("Phone No.: " + deliveryObjectList.FirstOrDefault().CustomerPhoneNumber, fontArial11)) { Colspan = 2, PaddingTop = 7f, PaddingBottom = 9f, PaddingLeft = 5f, PaddingRight = 5f });
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(new Phrase("Address: " + deliveryObjectList.FirstOrDefault().CustomerAddress, fontArial11)) { Colspan = 4, PaddingTop = 7f, PaddingBottom = 9f, PaddingLeft = 5f, PaddingRight = 5f });
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(new Phrase("Contact Person: " + deliveryObjectList.FirstOrDefault().ContactPerson, fontArial11)) { Colspan = 2, PaddingTop = 7f, PaddingBottom = 9f, PaddingLeft = 5f, PaddingRight = 5f });
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(new Phrase("Phone No.: " + deliveryObjectList.FirstOrDefault().ContactPersonPhoneNumber, fontArial11)) { Colspan = 2, PaddingTop = 7f, PaddingBottom = 9f, PaddingLeft = 5f, PaddingRight = 5f });
+
+                List<String> productListArray = new List<String>();
+                for (var i = 0; i < deliveryObjectList.FirstOrDefault().ProductLists.Count(); i++)
+                {
+                    productListArray.Add(deliveryObjectList.FirstOrDefault().ProductLists[i].ProductDescription);
+                }
+
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(new Phrase("Product/Service: " + String.Join(",", productListArray), fontArial11)) { Colspan = 4, PaddingTop = 7f, PaddingBottom = 9f, PaddingLeft = 5f, PaddingRight = 5f });
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(new Phrase("Particulars: " + deliveryObjectList.FirstOrDefault().Particulars, fontArial11)) { Colspan = 4, PaddingTop = 7f, PaddingBottom = 9f, PaddingLeft = 5f, PaddingRight = 5f, FixedHeight = 70f });
+
+                // table data for checklist
+                PdfPTable checkListTableData = new PdfPTable(2);
+                float[] kcheckListTableDataWithCells = new float[] { 5f, 95f };
+                checkListTableData.SetWidths(kcheckListTableDataWithCells);
+                checkListTableData.WidthPercentage = 100;
+                checkListTableData.AddCell(new PdfPCell(new Phrase("Checklist: ", fontArial11)) { Colspan = 2, Border = 0, PaddingTop = 4f, PaddingBottom = 6f, PaddingLeft = 5f, PaddingRight = 5f });
+
+                foreach (var checklistvalue in deliveryObjectList.FirstOrDefault().CheckLists)
+                {
+                    PdfContentByte cb = writer.DirectContent;
+                    PdfTemplate checkboxtemplate = cb.CreateTemplate(30, 30);
+                    checkboxtemplate.SetLineWidth(0.5f);
+                    checkboxtemplate.Rectangle(0, 0, 17f, 17f);
+                    checkboxtemplate.Stroke();
+
+                    Image checkboximage = Image.GetInstance(checkboxtemplate);
+                    checkboximage.ScalePercent(53f);
+                    PdfPCell checkboximagecell = new PdfPCell(checkboximage);
+
+                    checkListTableData.AddCell(new PdfPCell(checkboximagecell) { HorizontalAlignment = 2, Border = 0, PaddingTop = 0f, PaddingBottom = 0f, PaddingLeft = 5f, PaddingRight = 5f });
+                    checkListTableData.AddCell(new PdfPCell(new Phrase(checklistvalue.CheckListDescription, fontArial11)) { Border = 0, PaddingTop = 4f, PaddingBottom = 4f, PaddingLeft = 2f, PaddingRight = 5f });
+                }
+
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(checkListTableData) { Colspan = 4, PaddingTop = 7f, PaddingBottom = 9f, PaddingLeft = 5f, PaddingRight = 5f });
+
+                // table data for terms
+                PdfPTable termsListData = new PdfPTable(2);
+                float[] termsListDataWithCells = new float[] { 5f, 95f };
+                termsListData.SetWidths(termsListDataWithCells);
+                termsListData.WidthPercentage = 100;
+                termsListData.AddCell(new PdfPCell(new Phrase("Terms: ", fontArial11)) { Colspan = 2, Border = 0, PaddingTop = 2f, PaddingBottom = 4f, PaddingLeft = 5f, PaddingRight = 5f });
+                termsListData.AddCell(new PdfPCell(new Phrase("•", fontArial11)) { HorizontalAlignment = 2, Border = 0, PaddingTop = 2f, PaddingBottom = 4f, PaddingLeft = 5f, PaddingRight = 5f });
+                termsListData.AddCell(new PdfPCell(new Phrase("This is to certify that the project has been handed over to Operation for implementation and delivery.  The customer now may contact " + deliveryObjectList.FirstOrDefault().FunctionalUser + " of Operation for any question related to the project.", fontArial11)) { Border = 0, PaddingTop = 2f, PaddingBottom = 4f, PaddingLeft = 5f, PaddingRight = 5f });
+                termsListData.AddCell(new PdfPCell(new Phrase("•", fontArial11)) { HorizontalAlignment = 2, Border = 0, PaddingTop = 2f, PaddingBottom = 4f, PaddingLeft = 5f, PaddingRight = 5f });
+                termsListData.AddCell(new PdfPCell(new Phrase("The one to sign this document will be the signatory of the Software Acceptance Document as well.", fontArial11)) { Border = 0, PaddingTop = 2f, PaddingBottom = 4f, PaddingLeft = 5f, PaddingRight = 5f });
+                
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(termsListData) { Colspan = 4, PaddingTop = 7f, PaddingBottom = 9f, PaddingLeft = 5f, PaddingRight = 5f });
+
+
+                // table data for sales users
+                PdfPTable salesUserListDataTable = new PdfPTable(1);
+                float[] salesUserListDataTableWithCells = new float[] { 100f };
+                salesUserListDataTable.SetWidths(salesUserListDataTableWithCells);
+                salesUserListDataTable.WidthPercentage = 100;
+                salesUserListDataTable.AddCell(new PdfPCell(new Phrase("Sales: ", fontArial11)) { Colspan = 2, Border = 0, PaddingTop = 4f, PaddingBottom = 50f, PaddingLeft = 5f, PaddingRight = 5f });
+                salesUserListDataTable.AddCell(new PdfPCell(new Phrase(deliveryObjectList.FirstOrDefault().SalesUser, fontArial11)) { HorizontalAlignment = 1, Border = 0, PaddingTop = 2f, PaddingBottom = 4f, PaddingLeft = 5f, PaddingRight = 5f });
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(salesUserListDataTable) { PaddingTop = 2f, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+
+                // table data for functional users
+                PdfPTable functionalUserListDataTable = new PdfPTable(1);
+                float[] functionalUserListDataTableWithCells = new float[] { 100f };
+                functionalUserListDataTable.SetWidths(functionalUserListDataTableWithCells);
+                functionalUserListDataTable.WidthPercentage = 100;
+                functionalUserListDataTable.AddCell(new PdfPCell(new Phrase("Functional: ", fontArial11)) { Colspan = 2, Border = 0, PaddingTop = 4f, PaddingBottom = 50f, PaddingLeft = 5f, PaddingRight = 5f });
+                functionalUserListDataTable.AddCell(new PdfPCell(new Phrase(deliveryObjectList.FirstOrDefault().FunctionalUser, fontArial11)) { HorizontalAlignment = 1, Border = 0, PaddingTop = 2f, PaddingBottom = 4f, PaddingLeft = 5f, PaddingRight = 5f });
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(functionalUserListDataTable) { PaddingTop = 2f, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+
+                // table data for technical users
+                PdfPTable technicalUserListDataTable = new PdfPTable(1);
+                float[] technicalUserListDataTableWithCells = new float[] { 100f };
+                technicalUserListDataTable.SetWidths(technicalUserListDataTableWithCells);
+                technicalUserListDataTable.WidthPercentage = 100;
+                technicalUserListDataTable.AddCell(new PdfPCell(new Phrase("Technical: ", fontArial11)) { Colspan = 2, Border = 0, PaddingTop = 4f, PaddingBottom = 50f, PaddingLeft = 5f, PaddingRight = 5f });
+                technicalUserListDataTable.AddCell(new PdfPCell(new Phrase(deliveryObjectList.FirstOrDefault().TechnicalUser, fontArial11)) { HorizontalAlignment = 1, Border = 0, PaddingTop = 2f, PaddingBottom = 4f, PaddingLeft = 5f, PaddingRight = 5f });
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(technicalUserListDataTable) { PaddingTop = 2f, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+
+                // table data for customer
+                PdfPTable customerUserListDataTable = new PdfPTable(1);
+                float[] customerUserListDataTableWithCells = new float[] { 100f };
+                customerUserListDataTable.SetWidths(customerUserListDataTableWithCells);
+                customerUserListDataTable.WidthPercentage = 100;
+                customerUserListDataTable.AddCell(new PdfPCell(new Phrase("Customer: ", fontArial11)) { Colspan = 2, Border = 0, PaddingTop = 4f, PaddingBottom = 50f, PaddingLeft = 5f, PaddingRight = 5f });
+                customerUserListDataTable.AddCell(new PdfPCell(new Phrase(deliveryObjectList.FirstOrDefault().CustomerUser, fontArial11)) { HorizontalAlignment = 1, Border = 0, PaddingTop = 2f, PaddingBottom = 4f, PaddingLeft = 5f, PaddingRight = 5f });
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(customerUserListDataTable) { PaddingTop = 2f, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(new Phrase("Signed over printed name", fontArial11)) { HorizontalAlignment = 1, PaddingTop = 2f, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(new Phrase("Signed over printed name", fontArial11)) { HorizontalAlignment = 1, PaddingTop = 2f, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(new Phrase("Signed over printed name", fontArial11)) { HorizontalAlignment = 1, PaddingTop = 2f, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+                kickOffDeliveryDataTable.AddCell(new PdfPCell(new Phrase("Signed over printed name", fontArial11)) { HorizontalAlignment = 1, PaddingTop = 2f, PaddingBottom = 5f, PaddingLeft = 5f, PaddingRight = 5f });
+
+                document.Add(kickOffDeliveryDataTable);
 
                 // Document close
                 document.Close();
